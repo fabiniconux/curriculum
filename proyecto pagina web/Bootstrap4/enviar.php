df0ab0a9a04f71a8374bd8b52d0d405c2dfbc57d
fabiniconux <74539493+fabiniconux@users.noreply.github.com>
2021-04-30 12:08:35 -0500

    Add files via upload

---
+++ proyecto pagina web/Bootstrap4/enviar.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['nombre'];
+$mail = $_POST['mail'];
+$phone = $_POST['numero'];
+
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'solucionesodooerp@gmail.com';
+$asunto = 'Mensaje de PRECOMPC';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
